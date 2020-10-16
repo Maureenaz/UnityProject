a23e7c63bc5986e8cf00d96a7eb2d6edd6da2389
Maureenaz <48756130+Maureenaz@users.noreply.github.com>
2020-10-16 13:11:02 +0200

    :3

---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody2D rb;
+
+    private bool isGround;
+    public Transform feetPos;
+    public LayerMask whatIsGround;
+    public float checkRadius;
+    public float jumpForce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float MoveInput = Input.GetAxis("Horizontal");
+        rb.velocity = new Vector2(MoveInput * speed, rb.velocity.y);
+    }
+
+    void Update()
+    {
+        isGround = Physics2D.OverlapCircle(feetPos.position, checkRadius, whatIsGround);
+
+        if (isGround == true && Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            rb.velocity = Vector2.up * jumpForce;
+            Debug.Log("up");
+        }
+    }
+}
---
+++ Assets/Scripts/TowerRotate.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerRotate : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float moveHrizontal = Input.GetAxis("Horizontal");
+        transform.Rotate(0, moveHrizontal * speed, 0);
+    }
+}
