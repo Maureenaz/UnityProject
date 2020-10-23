1a50d68d1faa0b335346c3f2f313faa4dd4a9e50
Maureenaz <48756130+Maureenaz@users.noreply.github.com>
2020-10-23 16:22:53 +0200

    :3

---
+++ Assets/Scripts/Spawn.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    public float spawnTime;
+    public float spawnStart;
+
+    public int OeilVolant;
+
+    public GameObject[] ennemy;
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            Debug.Log("spawn");
+
+           StartCoroutine(coroutine());
+        }
+    }
+
+    IEnumerator coroutine()
+    {
+        while (true)
+        {
+            Debug.Log("start coroutine at : " + Time.time);
+
+            for (int i = 0; i < OeilVolant; i++)
+            {
+                int OeilIndex = Random.Range(0, ennemy.Length);
+                Instantiate(ennemy[OeilIndex], transform.position, transform.rotation);
+
+                yield return new WaitForSeconds(5);
+                Debug.Log("Spawn);
+            }
+        }
+    }
+
+    //void spawn()
+    //{
+    //    OeilVolant = Random.Range(0, ennemy.Length);
+    //    Instantiate(ennemy[OeilVolant, transform.position, transform.rotation]);
+    //}
+}
